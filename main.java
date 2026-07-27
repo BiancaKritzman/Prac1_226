6e07af7dfa67205a9050d6e63ce1c8b95d253688
BiancaKritzman <biancakritzman@gmail.com>
2026-07-27 16:23:28 +0200

    Added code

--- main.java
+++ main.java
@@ -1,3 +1,13 @@
 public class main {
-    
+
+   
+
+    public static void main(String[] args) {
+
+        LockOne l1 = new LockOne();
+        LockOne l2 = new LockOne();
+        l1.lock();
+
+
+    }
 }
