c1476e69836cd3f66725557a1639c79293313d62
BiancaKritzman <biancakritzman@gmail.com>
2026-07-28 09:32:53 +0200

    Added testing code in main

--- main.java
+++ main.java
@@ -8,6 +8,15 @@ public class main {
         LockOne l2 = new LockOne();
         l1.lock();
 
+        l2.lock(); //can't happen because l1 is locked
+
+        if(l1.flag[0] && l2.flag[1]){ {
+            System.out.println("Both threads are locked");
+        } else {
+            System.out.println("One of the threads is locked");
+        }
+
+        System.out.println("Thread 1 is locked and thread 2 is waiting for it to unlock");
 
     }
 }
