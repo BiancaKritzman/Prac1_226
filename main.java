e8976e24637a9f15a0bde889c48c793bf4d11c1e
Hibikus <hayleyjadenel@gmail.com>
2026-07-28 08:24:18 +0200

    LockTwo testing

--- main.java
+++ main.java
@@ -8,6 +8,22 @@ public class main {
         LockOne l2 = new LockOne();
         l1.lock();
 
+        //LockTwo testing
+        LockTwo lock = new LockTwo();
 
+        Thread t0 = new Thread(() -> {
+            System.out.println("Thread 0 attempts to acquire lock");
+            lock.lock(0);
+            lock.unlock();
+        });
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread 1 attempts to acquire lock");
+            lock.lock(1);
+            lock.unlock();
+        });
+
+        t0.start();
+        t1.start();
     }
 }
