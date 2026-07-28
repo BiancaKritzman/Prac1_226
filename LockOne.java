4b14341db87d9df158a69dad3fe36a43a8b7e111
BiancaKritzman <biancakritzman@gmail.com>
2026-07-28 07:55:59 +0200

    Fixed threadId

--- LockOne.java
+++ LockOne.java
@@ -4,7 +4,7 @@ class LockOne {
     private boolean[] flag = new boolean[2];
 
     public void lock(){
-        int i = (int) Thread.currentThread().getId();
+        int i = (int) Thread.currentThread().threadId();
         int j = 1 - i;
         flag[i] = true;
         
@@ -14,7 +14,7 @@ class LockOne {
     }
 
     public void unlock(){
-        int i = (int) Thread.currentThread().getId();
+        int i = (int) Thread.currentThread().threadId();
     
         if(flag[i]){
             flag[i] = false;
