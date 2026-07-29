3e3d83546c9e063a23bfb6c9a763fe4004c954b2
Hibikus <hayleyjadenel@gmail.com>
2026-07-29 15:15:27 +0200

    LockTwo done

--- LockTwo.java
+++ LockTwo.java
@@ -1,16 +1,27 @@
 interface Lock {
     void lock(int ID);
-    void unlock();
+    void unlock(int ID);
 }
 
 public class LockTwo implements Lock{
-    private volatile int victim; //decides who waits
+    private volatile int victim = -1; //decides who waits
     public void lock(int ID){
         int i = ID;
         victim = i;
         System.out.println("Thread "+ ID + " is the victim");
-        while (victim == i){} //wait
-        System.out.println("Thread "+ ID + " is in the critical section");
+        while (victim == i){
+            if (Thread.currentThread().isInterrupted()) {
+            System.out.println("Releasing Thread " + ID + " from hanging"); 
+            return; //a way to get out of the loop and  continue the demo
+            }
+        } //wait
+        System.out.println("Thread "+ ID + " enters the critical section");
     }
-    public void unlock(){};
+    public void unlock(int ID){
+        if (victim != ID)
+        {
+            System.out.println("Thread "+ ID + " exits the critical section");
+        }
+        victim = -1; //resets victim releasing the waiting thread
+    };
 }
